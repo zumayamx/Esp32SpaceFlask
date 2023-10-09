a385a38a997c0d7a7b1e9989d0c19797fadd6d81
rrayatec <79470126+rrayatec@users.noreply.github.com>
2023-10-09 08:31:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, jsonify
 from datetime import date, datetime, timedelta
 import mysql.connector
+from flask import Flask, render_template
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
@@ -16,7 +20,7 @@ def createConnection(user_name, database_name, user_password, host, port):
 def get_sensor_data():
     # Create a connection to the database
     cnx, cursor = createConnection(
-            '..', '..', '..', 'localhost', '3306')
+            'sql10651035', 'sql10651035', 'tkQyMRXggg', 'sql10.freemysqlhosting.net', '3306')
 
     # Query the database
     query = ("SELECT * FROM dht_sensor_data")
@@ -31,8 +35,30 @@ def get_sensor_data():
     cursor.close()
     cnx.close()
 
+        # Obtener los valores de x e y desde los datos
+    x = [item[0] for item in data]
+    y1 = [item[1] for item in data]
+    y2 = [item[2] for item in data]
+
+    # Crear la gráfica
+    plt.figure(figsize=(8, 4))
+    plt.plot(x, y1, label='Valor 1')
+    plt.plot(x, y2, label='Valor 2')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+
+    # Guardar la gráfica en un archivo temporal
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    img_data = base64.b64encode(img.getvalue()).decode()
+
+    # Renderizar la plantilla HTML con la gráfica
+    return render_template('index.html', img_data=img_data)
+
     # Return the data
-    return jsonify(data), 200
+    # return , 200
 
 
 @app.route('/sensor_data', methods=['POST'])
