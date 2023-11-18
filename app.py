b8815ec7aa46eef995634fd42d429b618b8412a0
Zumayaa <145822426+zumayamx@users.noreply.github.com>
2023-11-18 20:29:41 +0000

    inicio

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import mysql
 from flask import Flask, request, render_template, jsonify
 from datetime import date, datetime, timedelta
 import mysql.connector
@@ -10,14 +9,11 @@ import base64
 app = Flask(__name__)
 
 
-
 def createConnection(user_name, database_name, user_password, host, port):
     cnx = mysql.connector.connect(
         user=user_name, database=database_name, password=user_password, host=host, port=port)
     cursor = cnx.cursor()
     return (cnx, cursor)
-
-
 
 
 @app.route('/', methods=['GET'])
@@ -72,15 +68,13 @@ def receive_sensor_data():
 
         humidity = data.get('humidity')
         temperature = data.get('temperature')
-        date_time = data.get('date_time')
-        ESP_name = data.get('ESP_name')
+        date_time = data.get('date_time') # "2021-08-08 12:00:00"
 
-        cnx, cursor = createConnection(
-            'sql10651035', 'sql10651035', 'tkQyMRXggg', 'sql10.freemysqlhosting.net', '3306')
+        cnx, cursor = createConnection('sql3662385', 'sql3662385', '9jxvCJC8Uu', 'sql3.freemysqlhosting.net', '3306')
 
-        add_data = (
-            "INSERT INTO dht_sensor_data (humidity, temperature, date_time, ESP_name) VALUES (%s, %s, %s, %s)")
-        cursor.execute(add_data, (humidity, temperature, date_time, ESP_name))
+        add_data = ("INSERT INTO dht_sensor_data (humidity, temperature, date_time) VALUES (%s, %s, %s)")
+        
+        cursor.execute(add_data, (humidity, temperature, date_time))
         cnx.commit()
         cursor.close()
         cnx.close()
@@ -88,3 +82,4 @@ def receive_sensor_data():
         return 'Data received successfully.', 200
     else:
         return 'Invalid content type. Expected application/json.', 400
+
